9bad5116551e6edb139b95b6daaf56acb12bf1bd
KeaneHamilton <45226627+KeaneHamilton@users.noreply.github.com>
2018-11-21 11:05:32 -0800

    Add files via upload

--- simpleosc.m
+++ simpleosc.m
@@ -2,7 +2,9 @@ fs = 44100; % Hz
 BPM =130;
 
 
-x = readmidi('SadMachine.mid');
+
+
+x = readmidi('%PLACE MIDI FILE NAME HERE WITH SINGLE QUOTES%');
 y = midiInfo(x);
 y(:,[1,2,4,7,8])= [];
 z = [y(:,1), y(:,3) - y(:,2)];
@@ -19,19 +21,24 @@ melody = [];
 beats2sec = @(b) b * (1/BPM) * 60;
 
 sawtone= @(l,t) repmat(linspace(-1,1,t*fs), 1, round(l/t));
+triangletone= @(l,t) repmat([linspace(-1,1,t*fs/2),linspace(-1,1,t*fs/2)], 1, round(l/t));
+sqrtone= @(l,t) repmat([ones(1, floor(t*fs/2)), -1*ones(1, ceil(t*fs/2))], 1, round(l/t));
+
 
 for iNote = 1 : numel(melodyBeats)
 freq = midifreq(melodyNotes(iNote));
 duration = melodyBeats(iNote);
 offset = (duration/(1/freq))-round(duration/(1/freq));
-waveform= sawtone(duration, 1/freq);
-D = linspace(1, 0.2,length(waveform));
-tone =amp * D .* waveform ;
+waveform= sqrtone(duration, 1/freq);
+Decay = linspace(1, 0.2,length(waveform));
+tone =amp * Decay .* waveform ;
 melody = [melody, tone];
 end
 
 
 audiowrite('melodytest.wav',melody, fs);
+
+
 
 
 
